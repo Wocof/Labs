b5a36a525d8907078dffcee4052a16967cd076bd
Wocof <ponchick76@mail.ru>
2025-10-06 15:39:17 +0300

    Update Lab1.java

--- Lab1.java
+++ Lab1.java
@@ -3,26 +3,26 @@ import java.lang.Math;
 public class Lab1 {
     public static void main(String[] args) {
 
-        short[] W = new short[11];
-        for (int i = 0; i < W.length; i++) {
-            W[i] = (short) (i + 5);
+        short[] w = new short[11];
+        for (int i = 0; i < w.length; i++) {
+            w[i] = (short) (i + 5);
         }
 
-        float[] X = new float[12];
-        for (int i = 0; i < X.length; i++) {
-            X[i] = (float) (Math.random() * 19 - 15);
+        float[] x = new float[12];
+        for (int i = 0; i < x.length; i++) {
+            x[i] = (float) (Math.random() * 19 - 15);
         }
 
-        float[][] W1 = new float[11][12];
-        for (int i = 0; i < W.length; i++) {
-            for (int j = 0; j < X.length; j++) {
-                W1[i][j] = CalculateElements(W[i],X[j]);
+        float[][] w1 = new float[11][12];
+        for (int i = 0; i < w.length; i++) {
+            for (int j = 0; j < x.length; j++) {
+                w1[i][j] = calculateelements(W[i],X[j]);
             }
         }
-        PrintMatrix(W1);
+        printmatrix(W1);
     }
 
-    public static float CalculateElements(short W, float X) {
+    public static float calculateelements(short W, float X) {
 
         if (W == 11) {
             return (float)Math.exp(Math.pow((1 - Math.exp(X)) / Math.log(Math.abs(X)), 2));}
@@ -33,11 +33,12 @@ public class Lab1 {
         else return (float)Math.asin(Math.pow(Math.sin(Math.cos(X)), 2));
     }
 
-    public static void PrintMatrix(float[][] W1) {
-        for (int i = 0; i < W1.length; i++) {
-            for (int j = 0; j < W1[i].length; j++) {
+    public static void printmatrix(float[][] W1) {
+        for (int i = 0; i < w1.length; i++) {
+            for (int j = 0; j < w1[i].length; j++) {
                 System.out.printf("%9.2f", W1[i][j]);}
             System.out.println();
         }
     }
+
 }

d029f87203b413c0ffb9ff366e3ceb3435882fec
Wocof <ponchick76@mail.ru>
2025-10-20 03:22:14 +0300

    Create Blizzard.java

---
+++ Lab2/Moves/Blizzard.java
@@ -0,0 +1,17 @@
+package Moves;
+import ru.ifmo.se.pokemon.*;
+
+public final class Blizzard extends SpecialMove {
+    public Blizzard() {
+        super(Type.ICE, 110, 70);
+    }
+
+    @Override protected void applyOppEffects(Pokemon p) {
+
+        p.addEffect(new Effect().chance(0.1).condition(Status.FREEZE));
+    }
+
+    @Override protected String describe() {
+        return "Использует Attacks.Blizzard";
+    }
+}

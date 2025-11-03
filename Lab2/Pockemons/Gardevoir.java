fc4c2f3d2c3ee1c453f17ffd0575e7517907555d
Wocof <ponchick76@mail.ru>
2025-11-03 06:28:57 +0300

    Update Gardevoir.java

--- Lab2/Pockemons/Gardevoir.java
+++ Lab2/Pockemons/Gardevoir.java
@@ -1,15 +1,13 @@
-package Pokemons;
-import Moves.*;
-import ru.ifmo.se.pokemon.*;
+package pokemons;
+import moves.*;
 
-public final class Gardevoir extends Pokemon {
+public final class Gardevoir extends Kirlia {
 
     public Gardevoir(String name, int level) {
 
         super(name, level);
-        setType(Type.PSYCHIC, Type.FAIRY);
         setStats(68, 65, 65, 125, 115, 80);
-        this.setMove(new ThunderWave(), new WilloWisp(), new Charm(), new Moonblast());
+        this.addMove(new Moonblast());
 
     }
 

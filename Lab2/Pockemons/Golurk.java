2c42f094552f5e200a46b36a065edee4facf3daa
Wocof <ponchick76@mail.ru>
2025-11-03 06:29:33 +0300

    Update Golurk.java

--- Lab2/Pockemons/Golurk.java
+++ Lab2/Pockemons/Golurk.java
@@ -1,15 +1,13 @@
-package Pokemons;
-import Moves.*;
-import ru.ifmo.se.pokemon.*;
+package pokemons;
+import moves.*;
 
-public final class Golurk extends Pokemon {
+public final class Golurk extends Golett {
 
     public Golurk(String name, int level) {
 
         super(name, level);
-        setType(Type.GROUND, Type.GHOST);
         setStats(89, 124, 80, 55, 80, 55);
-        this.setMove(new HammerArm(), new Rest(), new IceBeam(), new StoneEdge());
+        this.setMove(new StoneEdge());
 
     }
 

996bece08505eb9f5ced0cb1d9bd7f99e268d760
Wocof <ponchick76@mail.ru>
2025-11-03 06:29:58 +0300

    Update Kirlia.java

--- Lab2/Pockemons/Kirlia.java
+++ Lab2/Pockemons/Kirlia.java
@@ -1,17 +1,14 @@
-package Pokemons;
-import Moves.*;
-import ru.ifmo.se.pokemon.*;
+package pokemons;
+import moves.*;
 
-public final class Kirlia extends Pokemon {
+public class Kirlia extends Ralts {
 
     public Kirlia(String name, int level) {
 
         super(name, level);
-        setType(Type.PSYCHIC, Type.FAIRY);
         setStats(38, 35, 35, 65, 55, 50);
-        this.setMove(new ThunderWave(), new WilloWisp(), new Charm());
+        this.addMove(new Charm());
 
     }
 
 }
-

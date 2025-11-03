d5954f663a96548f64042187513f29440faf4f2c
Wocof <ponchick76@mail.ru>
2025-11-03 06:34:21 +0300

    Update HammerArm.java

--- Lab2/Moves/HammerArm.java
+++ Lab2/Moves/HammerArm.java
@@ -1,4 +1,4 @@
-package Moves;
+package moves;
 import ru.ifmo.se.pokemon.*;
 
 public final class HammerArm extends PhysicalMove {
@@ -13,4 +13,5 @@ public final class HammerArm extends PhysicalMove {
     @Override protected String describe() {
         return "Использует Hammer Arm";
     }
+
 }
